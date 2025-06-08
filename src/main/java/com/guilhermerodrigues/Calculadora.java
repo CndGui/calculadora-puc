67193ef954315aec4522bb3c838f1a81b029f18c
Guilherme <work.guilherme@hotmail.com>
2025-06-08 10:49:51 -0300

    adicionar as operacoes de soma, subtracao, multiplicacao e divisao

--- src/main/java/com/guilhermerodrigues/Calculadora.java
+++ src/main/java/com/guilhermerodrigues/Calculadora.java
@@ -1,5 +1,19 @@
 package com.guilhermerodrigues;
 
 public class Calculadora {
+    public double Somar(double a, double b) {
+        return a + b;
+    }
 
+    public double Subtrair(double a, double b) {
+        return a - b;
+    }
+
+    public double Multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public double Dividir(double a, double b) {
+        return a / b;
+    }
 }
